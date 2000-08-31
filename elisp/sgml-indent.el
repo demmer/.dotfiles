7d445a445e1f84d2f2e12b8864ff59dcc810d39a
demmer <demmer>
2000-08-31 18:28:13 +0000

    Fix comment support, don't retreat past column 0, clean up comments.

--- elisp/sgml-indent.el
+++ elisp/sgml-indent.el
@@ -1,8 +1,8 @@
-;
-; sgml-indent.el
-;
-; Defines a simple indentation routine for editing sgml or other
-; derived file types. 
+;;
+;; sgml-indent.el
+;;
+;; Defines a simple indentation routine for editing sgml or other
+;; derived file types.
 
 (require 'sgml-mode)
 
@@ -32,6 +32,7 @@
   (back-to-indentation)
   (let ((indent-point (point))
 	(indent-column 0))
+    ;; Find the previous tag
     (when (re-search-backward "^\\([ \t]*\\)<" nil t)
       (let ((prev-indent-point (match-end 1))
 	    (prev-indent-column nil)
@@ -39,35 +40,37 @@
 	(goto-char prev-indent-point)
 	(setq prev-indent-column (current-column))
 
-					; debug
+	;; debug
 	(save-excursion
 	  (setq beginning (point))
 	  (end-of-line)
 	  (message "checking %s" (buffer-substring beginning (point))))
 	
-					; match the tag and any attributes
+	;; match the tag and any attributes
 	(cond
 	 ((re-search-forward "<[^/][^> \t]*[ \t]*\\([a-zA-Z_-]*=\".*\"[ \t\n\r]*\\)*"
 			    indent-point t)
-	  (message (format "tag matched %s" (match-string 0)))
+	  (message (format "tag matched \"%s\"" (match-string 0)))
+	  (message (format "looking-at returned %s" (looking-at ">.*-->")))
 	  (goto-char (match-end 0))
 	  (cond
-	   ((looking-at "/>")
+	   ((or
+	     (looking-at "/>")
+	     (looking-at ">.*</[^ \t]*>")
+	     (and (string-equal (match-string 0) "<!-- ")
+		  (looking-at ".*-->")))
+	    
 	    (message "Found self-closed tag")
-					; self-closed tag - indent to same level as previous tag
-	    (setq indent-column prev-indent-column)
-	    )
-	   ((looking-at ">.*</[^ \t]*>")
-	    (message "Found self-closed tag")
+	    ;; self-closed tag or comment - indent to same level as previous tag
 	    (setq indent-column prev-indent-column)
 	    )
 	   ((looking-at ">")
 	    (message "Found open tag")
-					; opened tag: increase indent
+	    ;; opened tag: increase indent
 	    (setq indent-column (+ prev-indent-column sgml-indent-level))
 	    )
 	   (t
-					; incomplete tag - indent to same level as first attribute
+	    ;; incomplete tag - indent to same level as first attribute
 	    (message "Found incomplete tag")
 	    (goto-char prev-indent-point)
 	    (re-search-forward "<[^ \t]*[ \t]*")
@@ -76,21 +79,22 @@
 	    )))
 
 	 ((looking-at "</[^ \t]*>")
-					; close tag - indent to same level as the tag
+	  ;; close tag - indent to same level as the tag
 	  (message "Found close tag")
 	  (setq indent-column prev-indent-column)
 	  )))
       )
       
-					; now need to check if the current line is a close tag
+    ;; now need to check if the current line is a close tag
     (goto-char indent-point)
     (if (looking-at "</[^ \t]*>")
 	(setq indent-column (- indent-column sgml-indent-level)))
       
     (message (format "indenting to column %d" indent-column))
+    (setq indent-column (max indent-column 0))
     (if (< indent-column (current-column))
 	(delete-char (- indent-column (current-column)))
-      (indent-to-column indent-column)
+      (indent-to-column (max indent-column 0))
       )))
 
 (provide 'sgml-indent)
