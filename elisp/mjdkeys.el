46c75fd72311ec99200bdcc891df7dae51e3c79e
miked <miked>
2003-03-12 19:09:46 +0000

    cosmetic cleanup, add M-? for grope

--- elisp/mjdkeys.el
+++ elisp/mjdkeys.el
@@ -1,9 +1,7 @@
-k;;;
+;;;
 ;;; Emacs Keybindings - mjd taken from amd
 ;;; 
 
-;(global-set-key "OC" 'forward-word)
-;(global-set-key "OD" 'backward-word)
 (global-set-key [(meta left)] 'backward-word)
 (global-set-key [(meta right)] 'forward-word)
 
@@ -24,11 +22,6 @@ k;;;
 (global-set-key [(f35)] 'scroll-up)
 
 
-(global-set-key "\M-g" 'goto-line)
-(global-set-key "\M-j" 'java-compile)
-(global-set-key "\M-a" 'java-compile-all)
-
-(global-set-key "\M-z" 'zwrite)
 
 ;;; amd: home=start, end = end
 (global-set-key [home] 'beginning-of-line)
@@ -42,16 +35,6 @@ k;;;
 
 ;;; amd: my tab shortcuts
 (global-set-key "\M-\\" 'indent-region)
-
-;;; amd: shift selections keys
-;(global-set-key [S-left] 'extend-region-left)
-;(global-set-key [S-right] 'extend-region-right)
-;(global-set-key [S-up] 'extend-region-up)
-;(global-set-key [S-down] 'extend-region-down)
-;(global-set-key [M-S-left] 'extend-region-word-left)
-;(global-set-key [M-S-right] 'extend-region-word-right)
-;(global-set-key [M-S-up] 'extend-region-para-up)
-;(global-set-key [M-S-down] 'extend-region-para-down)
 
 ; amd: control movement keys
 (global-set-key [(control left)] 'beginning-of-line)
@@ -78,26 +61,19 @@ k;;;
 (global-set-key [(f19)] 'next-error)
 (global-set-key [(f20)] 'goto-line)
 
-;;; mjd: annoying C-x C-u
-(global-set-key "\C-x\C-u" 'advertised-undo)
-
-;;; mjd: make M-TAB complete
-(global-set-key "\M-\t" 'dabbrev-expand)
-
 ;;; mjd: switch-CH-buffer I like as M-SPC and FRONT
 (global-set-key "\M- " 'switch-CH-buffer)
 (global-set-key [(f15)] 'switch-CH-buffer)
 
-;;; mjd: reverting is cool...
+;;; mjd: my handy settings
+(global-set-key "\M-g" 'goto-line)
+(global-set-key "\C-x\C-u" 'advertised-undo)
+(global-set-key "\M-\t" 'dabbrev-expand)
 (global-set-key "\M-r" 'revert-buffer-at-point)
 (global-set-key "\M-1" 'line-to-top-of-window)
-;; (global-set-key "\C-x\C-q" 'toggle-and-chmod-read-only)
-
-;; handy
 (global-set-key "\^[!" 'line-to-top-of-window)
 (global-set-key [(control return)] 'gnuserv-select-frame)
 (global-set-key "\C-x\C-x" 'pop-global-mark)
-
 (global-set-key "\C-cn" 'next-error)
 (global-set-key "\M-a" 'auto-fill-mode)
 (global-set-key "\M-p" 'vc-print-log)
@@ -106,6 +82,7 @@ k;;;
 (global-set-key "\M-\C-l" 'lcvs-examine)
 (global-set-key "\C-l" 'lcvs-examine)
 (global-set-key "\C-xO" 'other-window-only)
+(global-set-key "\M-?" 'grope)
 
 (global-set-key "\C-x\C-c" (lambda () (interactive) 
 			     (if (y-or-n-p "Really quit? ")
