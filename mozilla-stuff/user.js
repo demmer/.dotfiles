051627ddb84949779d1daa2f5791fa50d6938e4f
demmer <demmer>
2003-12-14 22:08:44 +0000

    *** empty log message ***

---
+++ mozilla-stuff/user.js
@@ -0,0 +1,10 @@
+
+// Instead of annoying error dialog messages, display pages: 
+user_pref("browser.xul.error_pages.enabled", true);
+
+// Instead of download progress windows, use the Sidebar: 
+user_pref("browser.download.openSidebar", true); 
+user_pref("browser.download.useProgressDialogs", false);
+
+// Change to normal Google search: 
+user_pref("keyword.URL", "http://www.google.com/search?btnG=Google+Search&q=");
