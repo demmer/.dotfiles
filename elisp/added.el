a07e627bf026fbbf257b5365a68e867f76f6f4ec
demmer <demmer>
2004-02-23 07:24:40 +0000

    add silly cs268 summary mail hook

--- elisp/added.el
+++ elisp/added.el
@@ -427,3 +427,21 @@ to match the current line in the source file."
 	  "~"))
 
 (setq make-backup-file-name-function 'my-make-backup-file-name)
+
+;; silly little hook for sending cs268 summaries
+(defun cs268-summary ()
+  (interactive)
+  (let ((name (file-name-nondirectory (buffer-file-name)))
+	(buf  (buffer-substring (point-min) (point-max))))
+    (string-match "[0-9]+-[0-9]+-\\(.*\\).txt" name)
+    (setq name (substring name (match-beginning 1) (match-end 1)))
+    (mail-other-window)
+    (insert "Ion Stoica <istoica@cs.berkeley.edu>")
+    (insert "\nBcc: demmer@cs.berkeley.edu")
+    (next-line)
+    (end-of-line)
+    (insert (format "cs268-Paper-Review: %s" name))
+    (goto-char (point-max))
+    (insert buf)
+    (goto-char (point-min))
+    ))
