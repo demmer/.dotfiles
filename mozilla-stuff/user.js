3c25ca2a2e184281100a4ae06cf9f9cf3dcf260a
demmer <demmer>
2004-08-25 17:24:38 +0000

    move prefs that are actually applicable from installation to installation from prefs.js to user.js

--- mozilla-stuff/user.js
+++ mozilla-stuff/user.js
@@ -1,4 +1,23 @@
 
+// path and download preferences
+user_pref("browser.startup.homepage", "http://my.yahoo.com/");
+user_pref("browser.download.dir", "/home/demmer/downloads");
+user_pref("browser.download.lastDir", "/tmp");
+user_pref("browser.download.openSidebar", true);
+user_pref("browser.download.useDownloadDir", false);
+user_pref("browser.download.useProgressDialogs", false);
+
+// ignore the annoying popup warnings
+user_pref("privacy.popups.firstTime", false);
+user_pref("security.warn_entering_secure", false);
+user_pref("security.warn_leaving_secure", false);
+user_pref("security.warn_submit_insecure", false);
+user_pref("security.warn_viewing_mixed", false);
+
+// tab bar prefs
+user_pref("browser.tabs.autoHide", false);
+user_pref("browser.tabs.warnOnClose", false);
+
 // Instead of annoying error dialog messages, display pages: 
 user_pref("browser.xul.error_pages.enabled", true);
 
