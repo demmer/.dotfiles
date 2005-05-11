96c386d5f508863bcb6762608e1e3f1e8c5a1c41
demmer <demmer>
2005-05-11 22:48:19 +0000

    don't try to start gnuserv (or load the file) unless there's an executable on the system

--- elisp/emacs-21-init.el
+++ elisp/emacs-21-init.el
@@ -121,15 +121,14 @@ This must be bound to a mouse-down event in the mode-line."
 ;; (append-no-dup 'time-stamp write-file-hooks)
 
 ;; start gnuserv (not emacsserver)
-(cond (window-system
-       (autoload 'gnuserv-start "gnuserv-compat" "server" t)
-       (message "starting gnuserv")
-       (cond ((or (file-executable-p "/usr/local/bin/gnuserv")
-		  (file-executable-p "/usr/bin/gnuserv"))
-	      (gnuserv-start)
-	      (start-process "gnuserv-keepalive" nil "gnuserv-keepalive")
-	 ))
-       ))
+(cond ((and window-system
+	    (or (file-executable-p "/usr/local/bin/gnuserv")
+		(file-executable-p "/usr/bin/gnuserv"))))
+      (autoload 'gnuserv-start "gnuserv-compat" "server" t)
+      (message "starting gnuserv")
+      (gnuserv-start)
+      (start-process "gnuserv-keepalive" nil "gnuserv-keepalive")
+      )
 
 ;; load in other files
 (load "modes")
