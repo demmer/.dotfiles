e3aea309e5237e881b1450547f7530b107aaeda2
demmer <demmer>
2010-03-15 18:29:17 +0000

    better rsync

--- bin/rsync_shadowfs.sh
+++ bin/rsync_shadowfs.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
 
-SUBDIR=$1/
+SUBDIR=$1
 shift
 if test -z $SUBDIR ; then
     echo "usage: $0 <SUBDIR>"
     exit 1
 fi
 
-SRCDIR=/Users/demmer/shadowfs_data/$SUBDIR
-#DSTDIR=/Volumes/mint-nfs/demmer/shadowfs_data/$SUBDIR
-DSTDIR=lime.lab.nbttech.com:/u/demmer/shadowfs_data/$SUBDIR
+if test -d /Users/$USER/shadowfs_data/$SUBDIR ; then
+    SUBDIR=$SUBDIR/
+fi
 
+ROOTDIR=`echo $SUBDIR | sed 's#/.*##'`
+
+SRCDIR=/Users/$USER/shadowfs_data/$SUBDIR
+DSTDIR=`readlink /Users/$USER/shadowfs_data/.config/$ROOTDIR | \
+        sed "s#$ROOTDIR#$SUBDIR#"`
+
+# XXX/demmer rsync over ssh seems to work better than using the Mac's
+# NFS stack
+DSTDIR=`echo $DSTDIR | sed 's#/Volumes/mint-nfs#lime.lab.nbttech.com:/u#'`
+
+if test -z $RSYNC ; then
+    RSYNC=rsync
+fi
 set -x
-rsync -rlpgoDvt --modify-window=1 --delete $* $SRCDIR $DSTDIR
+$RSYNC -rlpgoDvt --cvs-exclude --exclude=".git" --modify-window=2 --delete $* $SRCDIR $DSTDIR
