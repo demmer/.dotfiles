9cb65e22a98db1778e644168dd1b73a45359eb2e
demmer <demmer>
2011-01-10 18:32:59 +0000

    Add err checking Use onyx Add PULL support Ignore .pyc

--- bin/rsync_shadowfs.sh
+++ bin/rsync_shadowfs.sh
@@ -13,16 +13,28 @@ fi
 
 ROOTDIR=`echo $SUBDIR | sed 's#/.*##'`
 
+if test ! -e /Users/$USER/shadowfs_data/.config/$ROOTDIR ; then
+    echo "No shadowfs target configured for dir $ROOTDIR"
+    exit 1
+fi
+
 SRCDIR=/Users/$USER/shadowfs_data/$SUBDIR
 DSTDIR=`readlink /Users/$USER/shadowfs_data/.config/$ROOTDIR | \
         sed "s#$ROOTDIR#$SUBDIR#"`
 
 # XXX/demmer rsync over ssh seems to work better than using the Mac's
 # NFS stack
-DSTDIR=`echo $DSTDIR | sed 's#/Volumes/mint-nfs#lime.lab.nbttech.com:/u#'`
+DSTDIR=`echo $DSTDIR | sed 's#/Volumes/mint-nfs#onyx.lab:/u#'`
 
 if test -z $RSYNC ; then
     RSYNC=rsync
 fi
+
+if test ! -z $PULL ; then
+   TMP=$SRCDIR
+   SRCDIR=$DSTDIR
+   DSTDIR=$TMP
+fi
+
 set -x
-$RSYNC -rlpgoDvt --cvs-exclude --exclude=".git" --modify-window=2 --delete $* $SRCDIR $DSTDIR
+$RSYNC -rlpgoDvt --cvs-exclude --exclude=".git" --exclude="*.pyc" --modify-window=2 --delete $* $SRCDIR $DSTDIR
